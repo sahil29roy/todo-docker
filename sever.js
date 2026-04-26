9bf6d7f122d45a43787d5fd6ffd4b24014ff71b9
sahil <sahil29roy@gmail.com>
2026-04-26 08:46:01 +0530

    connect mongodb

--- sever.js
+++ sever.js
@@ -1,7 +1,8 @@
 import app from './app.js';
-
-const PORT = 3000;
+import connectDB from './config/db.js';
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`Server is listening at port ${PORT}`);
 });
